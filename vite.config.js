750337e11dd5bbc859db8cb7fc1427bde23487bc
Kaleemullah <kaleemullahahsan0@gmail.com>
2025-07-12 11:10:38 +0500

    vite-config updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteCompression from 'vite-plugin-compression'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240, // compress files bigger than 10kb
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+  ],
 })
